98a8fc78b8cf3de229dbe5d44a145c7cf7c380a9
yugarte9 <96316427+yugarte9@users.noreply.github.com>
2022-02-25 18:42:32 -0600

    Create helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,10 @@
 import sys
 
 def main():
-    print("Hello World! From Python: " + str(sys.version_info))
+    print("Hello World! From Phyton: " + str(sys.version_info))
     if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
-        # let's make this script fail for Python 3.6
-        raise Exception('Python version 3.6.x is unsupported!')
+        # let's make this script fail for Phyton 3.6
+        raise Exception('Phyton version 3.6.x is unsupported!')
 
 if __name__ == "__main__":
     main()
